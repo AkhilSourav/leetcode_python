82b8a123deeae53594c5b3b4b50c9bfa423355a9
Akhil <akhil.s18@iiits.in>
2023-04-02 21:03:34 +0530

    Maximum Depth of Binary Tree

---
+++ Practise Python/Trees/Trees Revision/maximum_depth_of_binary_tree.py
@@ -0,0 +1,66 @@
+# Created a class Node with init function, 
+# We can use this class to create a node with data and left and right child as None
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+    
+# Inserting a node in the Binary Search tree
+def insert(self,data):
+    if self is None:
+        return
+    
+    # If self.data is not None
+    if self.data:
+        # If data is less than self.data, then insert it in the left subtree
+        if data < self.data:
+            # If self.left is None, then create a new node and insert the data
+            if self.left is None:
+                self.left = Node(data)
+                # If self.left is not None, then insert the data in the left subtree
+            else:
+                insert(self.left,data)
+        # If data is greater than self.data, then insert it in the right subtree
+        elif data > self.data:
+            # If self.right is None, then create a new node and insert the data
+            if self.right is None:
+                self.right = Node(data)
+                # If self.right is not None, then insert the data in the right subtree
+            else:
+                insert(self.right,data)
+
+# Getting the maximum depth of the binary tree
+# Formula: 1 + max(LeftSubtree Height, RightSubtree Height)
+def maximum_depth_of_binary_tree(self) -> int:
+    if self is None:
+        return 0
+    
+    # Getting the height of the left subtree
+    lh = maximum_depth_of_binary_tree(self.left)
+    # Getting the height of the right subtree
+    rh = maximum_depth_of_binary_tree(self.right)
+    
+    # Returning the maximum depth of the binary tree
+    # 1 is added because we are counting the current node also
+    return 1 + max(lh,rh)
+
+
+root = Node(1)
+insert(root,6)
+insert(root,2)
+insert(root,3)
+insert(root,10)
+insert(root,8)
+insert(root,4)
+insert(root,5)
+insert(root,9)
+insert(root,7)
+
+# Printing the maximum depth of the binary tree
+print(f"Maximum depth of the binary tree is: {maximum_depth_of_binary_tree(root)}")
+
+
+ 
+        
+        
