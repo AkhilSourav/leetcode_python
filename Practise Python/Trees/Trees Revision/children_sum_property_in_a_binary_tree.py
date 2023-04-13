48a1bb34770a52860dc04437b22f8b6e4239a69f
Akhil <akhil.s18@iiits.in>
2023-04-13 19:04:04 +0530

    chidren sum property in a binary tree

--- Practise Python/Trees/Trees Revision/children_sum_property_in_a_binary_tree.py
+++ Practise Python/Trees/Trees Revision/children_sum_property_in_a_binary_tree.py
@@ -84,7 +84,7 @@ def modified_preorder_traversal(root):
 # Function to return satisfied children sum property binary tree
 def children_sum_property_satisfied(root):
     modified_preorder_traversal(root)
-    print(f"Chidren Sum Property {BFS(root)}")
+    print(f"Children Sum Property {BFS(root)}")
 
   
 # Inserting a node in the Binary Search tree
